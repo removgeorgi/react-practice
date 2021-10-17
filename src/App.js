e7e0947b00adf67a9af160750a57b8ecd541dd7b
removgeorgi <remov.georgi@gmail.com>
2021-10-17 20:07:36 +0300

    Added new Item component

--- src/App.js
+++ src/App.js
@@ -2,26 +2,27 @@ import './App.css';
 import * as React from 'react';
 import ReactDOM from 'react-dom';
 
-const list = [
-  {
-    title:'React',
-    url:'https://reactjs.org/',
-    author:'Jordan Walke',
-    num_comments:3,
-    points:4,
-    objectID:0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
 
-const App = () => (
+const App = () => {
+  const stories = [
+    {
+      title:'React',
+      url:'https://reactjs.org/',
+      author:'Jordan Walke',
+      num_comments:3,
+      points:4,
+      objectID:0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+    return (
     <div>
       <h1>My hacker stories</h1>
 
@@ -29,26 +30,17 @@ const App = () => (
       
     <hr />
 
-  <List />
+  <List list={stories} />
 
     </div>
   );
-  
+    }
 
-const List = () => (
-  <ul>
-          {list.map((item)=>{
-          return (
-          <li key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-          </li>
-          );
-        })}
+const List = (props) => (
+      <ul>
+          {props.list.map((item)=>(
+            <Item key={item.objectID} item={item} />
+          ))}
       </ul>
 );
 
@@ -57,6 +49,10 @@ const Search = () => {
 
     function handleChange(event){
       console.log(event);
+    }
+
+    function handleCount(delta){
+      setCount(count + delta);
     }
 
     function handleCLick(){
@@ -71,22 +67,30 @@ const Search = () => {
       {/* onChange attribute is needed first to add logging events of what was inputted
       and second to add synthetic events which prevents native browser behavior
       (e.g. refreshing a page after the user clicks a form's submit button) */}
-    <button id="search" type="button" onClick={handleCLick} >Search</button>
-  </div>
-  <div>
     Count: {count}
-    <button
-      type="button"
-      onClick={function(){
-        setCount(count + 1);
-      }}
-    >
-    Test
+    
+    <button id="search" type="button" onClick= {() => handleCount(1)} >
+    Increase count
     </button>
+    <button id="search" type="button" onClick= {() => handleCount(-1)} >
+    Decrease count
+    </button>
+
   </div>  
   </div>
   );
 };
+
+const Item = (props) => (
+  <li key={props.item.objectID}>
+            <span>
+              <a href={props.item.url}>{props.item.title}</a>
+            </span>
+            <span>{props.item.author}</span>
+            <span>{props.item.num_comments}</span>
+            <span>{props.item.points}</span>
+  </li>
+);
 
 
 export default App;
