7d4dea7656aabfdb9df62f703e00c190416af8ba
removgeorgi <remov.georgi@gmail.com>
2021-11-24 20:49:46 +0200

    making components reusable

--- src/App.js
+++ src/App.js
@@ -2,95 +2,74 @@ import './App.css';
 import * as React from 'react';
 import ReactDOM from 'react-dom';
 
-
 const App = () => {
-  const stories = [
-    {
-      title:'React',
-      url:'https://reactjs.org/',
-      author:'Jordan Walke',
-      num_comments:3,
-      points:4,
-      objectID:0,
-    },
-    {
-      title: 'Redux',
-      url: 'https://redux.js.org/',
-      author: 'Dan Abramov, Andrew Clark',
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-    }
-  ];
-    const [searchTerm,setSearchTerm] = React.useState(localStorage.getItem('search') || 'React');
-  
-    const handleSearch = (event) => {
-    setSearchTerm(event.target.value);
+	const stories = [
+		{
+			title: 'React',
+			url: 'https://reactjs.org/',
+			author: 'Jordan Walke',
+			num_comments: 3,
+			points: 4,
+			objectID: 0
+		},
+		{
+			title: 'Redux',
+			url: 'https://redux.js.org/',
+			author: 'Dan Abramov, Andrew Clark',
+			num_comments: 2,
+			points: 5,
+			objectID: 1
+		}
+	];
 
-    localStorage.setItem('search', event.target.value)
-    }
+	const [ searchTerm, setSearchTerm ] = React.useState(localStorage.getItem('search') || 'React');
 
-    React.useEffect(() => {
-      localStorage.setItem('searh',searchTerm)
-    },[searchTerm]);
+	const handleSearch = (event) => {
+		setSearchTerm(event.target.value);
 
-    const searchedStories = stories.filter((story)=>
-    story.title.toLowerCase().includes(searchTerm.toLowerCase()));
+		localStorage.setItem('search', event.target.value);
+	};
 
-    
-  
-    // const handleSearch = (event) => {
-    //   console.log(event.target.value);
-    // };
+	const searchedStories = stories.filter((story) => story.title.toLowerCase().includes(searchTerm.toLowerCase()));
 
-    return (
-    <div>
-      <h1>My hacker stories</h1>
+	React.useEffect(() => {
+		localStorage.setItem('search', searchTerm);
+	}, []);
 
-  <Search search ={searchTerm} onSearch={handleSearch} />
-      
-    <hr />
+	return (
+		<div>
+			<h1>My hacker stories</h1>
 
-  <List list={searchedStories} />
+			<InputWithLabel id="search" label="Search" value={searchTerm} onInputChange={handleSearch} />
 
-    </div>
-  );
+			<hr />
+
+			<List list={searchedStories} />
+		</div>
+	);
 };
 
-const List = (props) => (
-      <ul>
-          {props.list.map((item)=>(
-            <Item key={item.objectID} item={item} />
-          ))}
-      </ul>
-);
-
-const Search = (props) => (
-     // const handleChange = (event)=>{
-    //   console.log(event.target.value);
-
-  
-    
-  <div>
-    <label htmlFor="search">Search:</label>
-    <input id="search" type="text" onChange={props.onSearch} value={props.search} />
-      {/* onChange attribute is needed first to add logging events of what was inputted
+const InputWithLabel = ({ id, label, value, type = 'text', onInputChange }) => (
+	<div>
+		<label htmlFor={id}>{label}</label>
+		<input id={id} type={type} value={value} onChange={onInputChange} />
+	</div>
+	/* onChange attribute is needed first to add logging events of what was inputted
       and second to add synthetic events which prevents native browser behavior
-      (e.g. refreshing a page after the user clicks a form's submit button) */}
-  </div>
+      (e.g. refreshing a page after the user clicks a form's submit button) */
 );
 
+const List = ({ list }) => <ul>{list.map((item) => <Item key={item.objectID} item={item} />)}</ul>;
 
-const Item = (props) => (
-  <li key={props.item.objectID}>
-            <span>
-              <a href={props.item.url}>{props.item.title}</a>
-            </span>
-            <span>{props.item.author}</span>
-            <span>{props.item.num_comments}</span>
-            <span>{props.item.points}</span>
-  </li>
+const Item = ({ item }) => (
+	<li>
+		<span>
+			<a href={item.url}>{item.title}</a>
+		</span>
+		<span>{item.author}</span>
+		<span>{item.num_comments}</span>
+		<span>{item.points}</span>
+	</li>
 );
 
 export default App;
-
